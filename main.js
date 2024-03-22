6d3b700a5f9466ff8006d14ccac8b2eaedbc4810
abbos-khamidov <khamidov3538@gmail.com>
2024-03-22 20:37:57 +0500

    added js

---
+++ main.js
@@ -0,0 +1,13 @@
+// git config --global user.name "username"
+// git config --global user.email "email"
+
+console.log('Hello, world!')
+
+// git init
+// git add filename
+// git commit -m 'your message'
+// git branch -M main
+// git remote add origin link
+// git push -u origin main
+
+// git status
